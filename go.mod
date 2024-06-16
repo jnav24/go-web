ee1ff019434392b52ed9710b2da4fc99074cf549
Justin Navarro <info@justinnavarro.net>
2024-06-16 18:16:48 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jnav24/go-web
+
+go 1.21.5
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,15 @@
+package handlers
+
+import (
+	"github.com/jnav24/go-web/pkg/render"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "about.page.tmpl")
+}
+
