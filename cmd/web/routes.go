162e2be7d0ce1a4847784e600254b7b1a877ae4f
Justin Navarro <info@justinnavarro.net>
2024-06-21 19:55:30 -0400

    trying out chi router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/jnav24/go-web/pkg/config"
 	"github.com/jnav24/go-web/pkg/handlers"
 	"net/http"
 )
 
-func routes(app *config.AppConfig) http.Handler {
+func useChiRouter() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
+
+func usePatRouter() http.Handler {
 	mux := pat.New()
 
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
 	return mux
+}
+
+func routes(app *config.AppConfig) http.Handler {
+	//return usePatRouter()
+	return useChiRouter()
 }
