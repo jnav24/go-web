10b13b7d30494f0529901f2232829c3ec71269f6
Justin Navarro <info@justinnavarro.net>
2024-06-22 18:22:45 -0400

    setting and getting values from session

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -24,12 +24,18 @@ func NewHandlers(r *Repository) {
 }
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := map[string]string{}
 	stringMap["test"] = "hello again"
+
+	remoteIp := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIp
 
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
