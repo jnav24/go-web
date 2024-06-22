ee3987d0cd99c7b0aff7a85e7697c6e1bbb58f61
Justin Navarro <info@justinnavarro.net>
2024-06-22 09:58:46 -0400

    set up session

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,8 +1,15 @@
 package config
 
-import "html/template"
+import (
+	"github.com/alexedwards/scs/v2"
+	"html/template"
+	"log"
+)
 
 type AppConfig struct {
 	TemplateCache map[string]*template.Template
 	UseCache      bool
+	InfoLog       *log.Logger
+	InProduction  bool
+	Session       *scs.SessionManager
 }
